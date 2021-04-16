c396545fa2e902766cd21fe0fceb0c3b9f9a610c
blueshark42 <roland.csibrei.jr@gmail.com>
2021-04-17 01:38:06 +0200

    File upload

--- src/network.cpp
+++ src/network.cpp
@@ -1,60 +1,103 @@
+#include <fstream>
 #include "network.h"
 
 BOOL ConnectToServer() {
-	WSADATA wsaData;
-	int result;
 
-	auto connSock = INVALID_SOCKET;
-	struct sockaddr_in clientService{};
-
-	char *sendbuf = "test test";
-	char recvbuf[2048];
-
-	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (result!=NO_ERROR) {
-		std::cout << "WSAStartup: " << result;
-		return EXIT_FAILURE;
-	}
-
-	connSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (connSock==INVALID_SOCKET) {
-		std::cout << "socket: " << WSAGetLastError();
-		WSACleanup();
-		return EXIT_FAILURE;
-	}
-
-	clientService.sin_family = AF_INET;
-	clientService.sin_addr.s_addr = inet_addr(IP);
-	clientService.sin_port = htons(PORT);
-
-	result = connect(connSock, (SOCKADDR *)&clientService, sizeof(clientService));
-	if (result==SOCKET_ERROR) {
-		CleanSocket(connSock);
-		return EXIT_FAILURE;
-	}
-
-	result = send(connSock, sendbuf, (int)strlen(sendbuf), 0);
-	if (result==SOCKET_ERROR) {
-		CleanSocket(connSock);
-		return EXIT_FAILURE;
-	}
-
-	do {
-		result = recv(connSock, recvbuf, 2048, 0);
-		if (result > 0) {
-			std::cout << "Received: " << result << std::endl;
-		} else if (result==0) {
-			std::cout << "Connection closed\n";
-		} else {
-			std::cout << "recv failed\n";
-		}
-	} while (result > 0);
-	CleanSocket(connSock);
-
-	return 0;
 }
 
-void CleanSocket(SOCKET socket) {
-	WSACleanup();
+void CleanSocket(SOCKET &socket) {
 	closesocket(socket);
+	socket = INVALID_SOCKET;
 }
+int64_t SendLogFile(SOCKET s, const std::string &file, int chunkSize) {
+	const int64_t fileSize = GetFileSize(file);
+	if (fileSize < 0) { return -1; }
+
+	std::ifstream iffile(file, std::ifstream::binary);
+	if (iffile.fail()) { return -1; }
+
+	if (SendBuffer(s, reinterpret_cast<const char *>(&fileSize), sizeof(fileSize))!=sizeof(fileSize)) {
+		return -2;
+	}
+
+	char *buffer = new char[chunkSize];
+	bool error = false;
+	int64_t i = fileSize;
+	while (i!=0) {
+		const int64_t ssize = __min(i, (int64_t)chunkSize);
+		if (!iffile.read(buffer, ssize)) {
+			error = true;
+			break;
+		}
+		const int l = SendBuffer(s, buffer, (int)ssize);
+		if (l < 0) {
+			error = true;
+			break;
+		}
+		i -= l;
+	}
+	delete[] buffer;
+	iffile.close();
+	return error ? -3 : fileSize;
+}
+
+int64_t GetFileSize(const std::string &file) {
+	FILE *f;
+	if (fopen_s(&f, file.c_str(), "rb")!=0) {
+		return -1;
+	}
+	_fseeki64(f, 0, SEEK_END);
+	const int64_t len = _ftelli64(f);
+	fclose(f);
+	return len;
+}
+
+int SendBuffer(SOCKET s, const char *buffer, int buffSize, int chunkSize) {
+	int i = 0;
+	while (i < buffSize) {
+		const int l = send(s, &buffer[i], __min(chunkSize, buffSize - i), 0);
+		if (l < 0) {
+			return l;
+		}
+		i += l;
+	}
+	return i;
+}
+DWORD ClientThread(LPVOID param) {
+	struct addrinfo hints = {0}, *result, *ptr;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	if (getaddrinfo(IP, PORT, &hints, &result)!=0) {
+		return -1;
+	}
+
+	auto client = INVALID_SOCKET;
+	for (ptr = result; ptr!=nullptr; ptr = ptr->ai_next) {
+		client = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (client==SOCKET_ERROR) {
+			CleanSocket(client);
+			return -1;
+		}
+		if (connect(client, ptr->ai_addr, (int)ptr->ai_addrlen)==SOCKET_ERROR) {
+			closesocket(client);
+			client = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(result);
+
+	if (client==SOCKET_ERROR) {
+		std::cout << "Could not create client socket\n";
+		return -2;
+	}
+
+	int64_t rc = SendFile(client, PATH);
+	if (rc < 0) {
+		std::cout << "Failed to send file: " << rc << std::endl;
+	}
+	closesocket(client);
+	return 0;
+};
--- src/network.h
+++ src/network.h
@@ -1,14 +1,21 @@
 #ifndef ENIGMA_SRC_NETWORK_H_
 #define ENIGMA_SRC_NETWORK_H_
-#include <iostream>
-#include <WS2tcpip.h>
-#include <WinSock2.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
+#include <iostream>
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+#include <fstream>
+
+
 #define IP "127.0.0.1"
-#define PORT 8080
+#define PORT "8080"
 
 BOOL ConnectToServer();
-void CleanSocket(SOCKET socket);
+DWORD __stdcall ClientThread(LPVOID param);
+void CleanSocket(SOCKET &socket);
+int64_t SendLogFile(SOCKET s, const std::string &file, int chunkSize = 64*1024);
+int64_t GetFileSize(const std::string &file);
+int SendBuffer(SOCKET s, const char *buffer, int buffSize, int chunkSize = 4*1024);
 #endif //ENIGMA_SRC_NETWORK_H_
